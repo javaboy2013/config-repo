cdcd26bc4c7522fbad255e5542a6ac71492b5adb
yy <yy@yy-PC.lan>
2017-06-21 14:48:46 +0800

    测试

---
+++ src/main/java/org/spring/cloud/config/repo/App.java
@@ -0,0 +1,13 @@
+package org.spring.cloud.config.repo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
